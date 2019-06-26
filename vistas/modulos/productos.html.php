28a1da82f2bdb2826b7788cfae0e29c246238c1c
gil <gramirez25@ucol.mx>
2019-06-26 16:11:02 -0500

    v43 Seleccionar productos

--- vistas/modulos/productos.html.php
+++ vistas/modulos/productos.html.php
@@ -52,55 +52,33 @@
 
           </thead>
 
-          <tbody>}
-
-            <?php 
-
-              $item = null;
-              $valor = null;
-
-              $productos = ControladorProductos::ctrMostrarProductos($item, $valor);
-
-              //var_dump($productos);
-
-              foreach ($productos as $key => $value) {
-                
-                echo '<tr>
-                
-                        <td>'.($key+1).'</td>
-                        <td><img src="vistas/img/productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
-                        <td>'.$value["codigo"].'</td>
-                        <td>'.$value["descripcion"].'</td>';
-
-                        $item = "id";
-                        $valor = $value["id_categoria"];
-
-                        $categoria = ControladorCategorias::ctrMostrarCategorias($item, $valor);
-
-                        echo '<td>'.$categoria["categoria"].'</td>
-                        <td>'.$value["stock"].'</td>
-                        <td>'.$value["precio_compra"].'</td>
-                        <td>'.$value["precio_venta"].'</td>
-                        <td>'.$value["fecha"].'</td>
-                        <td>
+          <tbody>
             
-                          <div class="btn-group">
-                            
-                            <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+            <tr>
+                
+              <td>1</td>
+              <td><img src="vistas/img/productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+              <td>0001</td>
+              <td>Lorem ipsum dolor sit amem</td>
+              <td>Lorem ipsum</td>
+              <td>20</td>
+              <td>$ 5.00</td>
+              <td>$ 10.00</td>
+              <td>2019-24-05 12:05:32</td>
+              <td>
+  
+                <div class="btn-group">
+                  
+                  <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
 
-                            <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                  <button class="btn btn-danger"><i class="fa fa-times"></i></button>
 
 
-                          </div>
+                </div>
 
-                        </td> 
+              </td> 
 
-                      </tr>';
-                      
-              }
-
-             ?>
-            
+            </tr>
 
           </tbody>
             
