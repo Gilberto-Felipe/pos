037e1d7d1bb9cd4ccaceb8b02f9c4d9a6c6f0036
gil <gramirez25@ucol.mx>
2019-06-27 18:08:48 -0500

    v44 y 45 Plugin Data-table dinámico, falta un video más para terminar el plugin

--- vistas/modulos/productos.php
+++ vistas/modulos/productos.php
@@ -31,7 +31,7 @@
 
       <div class="box-body">
 
-        <table class="table table-bordered table-striped dt-responsive tablas">
+        <table class="table table-bordered table-striped dt-responsive tablaProductos" width="100%">
 
           <thead> 
 
@@ -51,58 +51,6 @@
             </tr>
 
           </thead>
-
-          <tbody>}
-
-            <?php 
-
-              $item = null;
-              $valor = null;
-
-              $productos = ControladorProductos::ctrMostrarProductos($item, $valor);
-
-              //var_dump($productos);
-
-              foreach ($productos as $key => $value) {
-                
-                echo '<tr>
-                
-                        <td>'.($key+1).'</td>
-                        <td><img src="vistas/img/productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
-                        <td>'.$value["codigo"].'</td>
-                        <td>'.$value["descripcion"].'</td>';
-
-                        $item = "id";
-                        $valor = $value["id_categoria"];
-
-                        $categoria = ControladorCategorias::ctrMostrarCategorias($item, $valor);
-
-                        echo '<td>'.$categoria["categoria"].'</td>
-                        <td>'.$value["stock"].'</td>
-                        <td>'.$value["precio_compra"].'</td>
-                        <td>'.$value["precio_venta"].'</td>
-                        <td>'.$value["fecha"].'</td>
-                        <td>
-            
-                          <div class="btn-group">
-                            
-                            <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
-
-                            <button class="btn btn-danger"><i class="fa fa-times"></i></button>
-
-
-                          </div>
-
-                        </td> 
-
-                      </tr>';
-                      
-              }
-
-             ?>
-            
-
-          </tbody>
             
         </table>
 
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -157,6 +157,7 @@ if(isset($_GET["ruta"])){
   <script src="vistas/js/plantilla.js"></script>
   <script src="vistas/js/usuarios.js"></script>
   <script src="vistas/js/categorias.js"></script>
+  <script src="vistas/js/productos.js"></script>
 
 </body>
 </html>
