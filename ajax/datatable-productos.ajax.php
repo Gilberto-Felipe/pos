b96d6bf86ea6f00de969513ec293e5824581f8c9
gil <gramirez25@ucol.mx>
2019-07-01 17:55:08 -0500

    v46 plugin datatable dinámico fin
    finalizé la configuración del datatble dinámico con html, js, ajax.php. Esta es una sección importante.

--- ajax/datatable-productos.ajax.php
+++ ajax/datatable-productos.ajax.php
@@ -19,23 +19,64 @@ class TablaProductos{
 
   		$productos = ControladorProductos::ctrMostrarProductos($item, $valor);
 
-		$botones = "<div class='btn-group'><button class='btn btn-warning'><i class='fa fa-pencil'></i></button><button class='btn btn-danger'><i class='fa fa-times'></i></button></div>";
-
   		$datosJson = '{
 			"data": [';
 
 			for($i = 0; $i < count($productos); $i++){
 
+			  	/*=============================================
+	 	 		TRAEMOS LA IMAGEN
+	  			=============================================*/ 
+
 				$imagen = "<img src='".$productos[$i]["imagen"]."' width='40px'>";
+
+			  	/*=============================================
+	 	 		ARREGLAMOS PROBLEMA DE " DENTRO DE LA DESCRIPCIÓN
+	  			=============================================*/ 
+
+				$descripcion = htmlspecialchars($productos[$i]["descripcion"], ENT_QUOTES, "UTF-8");
+
+			  	/*=============================================
+	 	 		TRAEMOS LA CATEGORÍA
+	  			=============================================*/ 
+
+				$item = "id";
+				$valor = $productos[$i]["id_categoria"];
+
+				$categorias = ControladorCategorias::ctrMostrarCategorias($item, $valor);
+
+				/*=============================================
+	 	 		STOCK
+	  			=============================================*/ 
+
+		  			if($productos[$i]["stock"] <= 10){
+
+		  				$stock = "<button class='btn btn-danger'>".$productos[$i]["stock"]."</button>";
+
+		  			}else if($productos[$i]["stock"] > 11 && $productos[$i]["stock"] <= 15){
+
+		  				$stock = "<button class='btn btn-warning'>".$productos[$i]["stock"]."</button>";
+
+		  			}else{
+
+		  				$stock = "<button class='btn btn-success'>".$productos[$i]["stock"]."</button>";
+
+		  			}
+
+			  	/*=============================================
+	 	 		TRAEMOS LAS ACCIONES = BOTONES EDITAR Y ELIMINAR
+	  			=============================================*/ 
+
+	  			$botones =  "<div class='btn-group'><button class='btn btn-warning btnEditarProducto' idProducto='".$productos[$i]["id"]."' data-toggle='modal' data-target='#modalEditarProducto'><i class='fa fa-pencil'></i></button><button class='btn btn-danger btnEliminarProducto' idProducto='".$productos[$i]["id"]."' codigo='".$productos[$i]["codigo"]."' imagen='".$productos[$i]["imagen"]."'><i class='fa fa-times'></i></button></div>"; 
 
 
 				$datosJson .= '[
 						  "'.($i+1).'",
 						  "'.$imagen.'",
 						  "'.$productos[$i]["codigo"].'",
-						  "'.$productos[$i]["descripcion"].'",
-						  "Taladros",
-						  "'.$productos[$i]["stock"].'",
+						  "'.$descripcion.'",
+						  "'.$categorias["categoria"].'",
+						  "'.$stock.'",
 						  "'.$productos[$i]["precio_compra"].'",
 						  "'.$productos[$i]["precio_venta"].'",
 						  "'.$productos[$i]["fecha"].'",
@@ -47,7 +88,8 @@ class TablaProductos{
 			$datosJson = substr($datosJson, 0, -1);
 
 			$datosJson .= ']
-			}';
+
+		}';
 
 		echo $datosJson;
 
--- controladores/productos.controlador.php
+++ controladores/productos.controlador.php
@@ -14,9 +14,6 @@ class ControladorProductos {
 
 		return $respuesta;
 
-
-
-
 	}
 
 }
